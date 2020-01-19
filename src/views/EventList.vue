595b4754cffef7627177b1583ea71cdbfbdbb845
Alexander Balashov <aleksandr.v@rcsmail.ru>
2020-01-19 15:34:47 +0300

    API Calls added

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
+import EventService from '@/services/EventService';
 import EventCard from '@/components/EventCard.vue';
 
 export default {
   components: {
     EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  async created() {
+    try {
+      const { data } = await EventService.getEvents();
+      this.events = data;
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 </script>
