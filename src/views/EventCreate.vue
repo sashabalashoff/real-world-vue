3486a496e1c7167a3d2eeb85d7157805d9330a10
Alexander Balashov <aleksandr.v@rcsmail.ru>
2020-01-27 15:33:13 +0300

    Event create

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    {{ user.name }}
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: mapState(['user', 'categories']),
+};
+</script>
+
+<style scoped>
+
+</style>
